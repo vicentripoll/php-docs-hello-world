ab59377ea8b9596e3df37ec35431f817371f35aa
vicentripoll <v.ripollriera@edu.gva.es>
2026-05-22 14:14:52 +0200

    Update storage.php

--- storage.php
+++ storage.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 
 // Configuración
 $connectionString = getenv("AZURE_STORAGE_CONNECTION_STRING");
-$containerName = "comprimidos";
+$containerName = "contenedor-blobs";
 
 if (!$connectionString) {
     die("La variable AZURE_STORAGE_CONNECTION_STRING no está configurada.");
